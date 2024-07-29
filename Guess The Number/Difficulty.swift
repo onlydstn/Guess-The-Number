b2acafb2c1d99dc2fd0e918272a1d33ccff819f0
Dustin Nuzzo <dustin.nuzzo@icloud.com>
2024-07-29 17:13:04 +0200

    initial commit

---
+++ Guess The Number/Difficulty.swift
@@ -0,0 +1,14 @@
+//
+//  Difficulty.swift
+//  Guess The Number
+//
+//  Created by Dustin Nuzzo on 29.07.24.
+//
+
+import Foundation
+
+enum Difficulty {
+    case easy
+    case medium
+    case hard
+}
---
+++ Guess The Number/EasyGame.swift
@@ -0,0 +1,29 @@
+//
+//  EasyGame.swift
+//  Guess The Number
+//
+//  Created by Dustin Nuzzo on 29.07.24.
+//
+
+func easyGame() {
+    let randomNumber: Int = Int.random(in: 1...10)
+    var tries: Int = 0
+    var solved: Bool = false
+    
+    print("Im thinking of a number between 1 and 10... can you guess it?")
+    while solved == false {
+        if let input = readLine() {
+            if input < String(randomNumber) {
+                tries += 1
+                print("Your number is too small. Tries: \(tries)")
+            } else if input > String(randomNumber) {
+                tries += 1
+                print("Your number is too big. Tries: \(tries)")
+            } else {
+                tries += 1
+                print("You guessed right! Tries: \(tries)")
+                solved = true
+            }
+        }
+    }
+}
